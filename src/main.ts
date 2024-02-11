ef94f738787d631962521a9c52688b52f664e7ee
Gnlow <gnlow@protonmail.com>
2024-02-12 01:36:35 +0900

    getSelection

--- src/main.ts
+++ src/main.ts
@@ -33,15 +33,40 @@ const pad = el("div", {
     ],
 })
 
+const getPos = (target: Node | null): number => {
+    if (!target) return 0
+    if (target == pad) return 0
+    
+    const prev = target.previousSibling
+    if (prev) {
+        return prev.textContent!.length + getPos(prev)
+    } else {
+        return getPos(target.parentNode)
+    }
+}
+
+const getSelection = () => {
+    const {
+        anchorNode,
+        anchorOffset,
+        focusNode,
+        focusOffset,
+    } = document.getSelection()!
+    return [
+        getPos(anchorNode) + anchorOffset,
+        getPos(focusNode) + focusOffset,
+    ]
+}
+
 document.addEventListener("selectionchange", () => {
-    console.log(document.getSelection())
+    console.log(getSelection())
 })
 
 const observer = new MutationObserver((mutations) => {
     mutations.forEach(({ type }) => {
         if (type == "characterData") {
             console.log(pad.textContent)
-            console.log(document.getSelection())
+            console.log(getSelection())
         }
     })
 })
