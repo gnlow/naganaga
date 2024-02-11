11f76eceeb781bde3141a80c5f0b9a091f09995f
Gnlow <gnlow@protonmail.com>
2024-02-11 20:41:32 +0900

    style

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
 import { parse } from "https://tsm.deno.dev/https://deno.land/std@0.215.0/csv/mod.ts"
 
-console.log(parse(await fetch("https://gsheet.deno.dev/1KuRQFwPnn4_jBlTkUHVeWD-6DRmtDohEBDsaP7XWxNY").then(x => x.text())))
+const data = parse(await fetch("https://gsheet.deno.dev/1KuRQFwPnn4_jBlTkUHVeWD-6DRmtDohEBDsaP7XWxNY").then(x => x.text()))
+
+type Attr = Record<string, string | (string | HTMLElement)[]>
+
+const el = (name: string, { children, ...attr }: Attr = {}) => {
+    const elem = document.createElement(name)
+
+    Object.entries(attr as Record<string, string>).forEach(([k, v]) => {
+        elem.setAttribute(k, v)
+    })
+    elem.append(...(children || []))
+
+    return elem
+}
+
+const pad = el("div", {
+    class: `
+        w(100%)
+        h(100%)
+        p(1rem)
+        font(64)
+    `,
+    contentEditable: "true",
+    children: ["Hi"],
+})
+
+document.body.append(pad)
