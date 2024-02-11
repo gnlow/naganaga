d0ada0b87ed2251a5123c834df040c93de7eb299
Gnlow <gnlow@protonmail.com>
2024-02-12 01:19:22 +0900

    selection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { parse } from "https://tsm.deno.dev/https://deno.land/std@0.215.0/csv/mod.ts"
 
-const data = parse(await fetch("https://gsheet.deno.dev/1KuRQFwPnn4_jBlTkUHVeWD-6DRmtDohEBDsaP7XWxNY").then(x => x.text()))
+// const data = parse(await fetch("https://gsheet.deno.dev/1KuRQFwPnn4_jBlTkUHVeWD-6DRmtDohEBDsaP7XWxNY").then(x => x.text()))
 
 type Attr = Record<string, string | (string | HTMLElement)[]>
 
@@ -33,10 +33,15 @@ const pad = el("div", {
     ],
 })
 
+document.addEventListener("selectionchange", () => {
+    console.log(document.getSelection())
+})
+
 const observer = new MutationObserver((mutations) => {
-    mutations.forEach(({ type, target }) => {
+    mutations.forEach(({ type }) => {
         if (type == "characterData") {
-            console.log(target.textContent)
+            console.log(pad.textContent)
+            console.log(document.getSelection())
         }
     })
 })
