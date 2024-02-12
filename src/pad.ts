6628e15f016bd5e02b7f4aa93ed74b33e8797747
Gnlow <gnlow@protonmail.com>
2024-02-12 22:45:38 +0900

    folder

---
+++ src/pad.ts
@@ -0,0 +1,71 @@
+import { el } from "./el.ts"
+
+import { parse } from "https://tsm.deno.dev/https://deno.land/std@0.215.0/csv/mod.ts"
+
+const data = parse(await fetch("https://gsheet.deno.dev/1KuRQFwPnn4_jBlTkUHVeWD-6DRmtDohEBDsaP7XWxNY").then(x => x.text()))
+console.log(data)
+
+export const pad = el("div", {
+    class: `
+        w(100%)
+        h(100%)
+        p(1rem)
+        font(64)
+    `,
+    contentEditable: "true",
+    children: [
+        "Hello, ",
+        el("span", {
+            class: "c(orange)",
+            children: "World",
+        }),
+        "!",
+    ],
+})
+
+const getPos = (target: Node | null): number => {
+    if (!target) return 0
+    if (target == pad) return 0
+    
+    const prev = target.previousSibling
+    if (prev) {
+        return prev.textContent!.length + getPos(prev)
+    } else {
+        return getPos(target.parentNode)
+    }
+}
+
+const getSelection = () => {
+    const {
+        anchorNode,
+        anchorOffset,
+        focusNode,
+        focusOffset,
+    } = document.getSelection()!
+    return [
+        getPos(anchorNode) + anchorOffset,
+        getPos(focusNode) + focusOffset,
+    ]
+}
+
+document.addEventListener("selectionchange", () => {
+    console.log(getSelection())
+})
+
+const normalize = (str: string) => str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach(({ type }) => {
+        if (type == "characterData") {
+            console.log(pad.textContent)
+            console.log(data.filter(([i, word]) =>
+                normalize(word).includes(normalize(pad.textContent!))
+            ))
+        }
+    })
+})
+
+observer.observe(pad, {
+    subtree: true,
+    characterData: true,
+})
