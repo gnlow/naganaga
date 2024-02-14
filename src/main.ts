7a0f6424f6334230c0dd62d416141f1477ac84e4
Gnlow <gnlow@protonmail.com>
2024-02-14 15:50:09 +0900

    search show

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { pad, onChange } from "./pad.ts"
 import { search } from "./search.ts"
 import { getSelection } from "./getSelection.ts"
+import { el } from "./el.ts"
+
+const $search = document.querySelector("#search")!
 
 document.querySelector("#pad")!.append(pad)
 onChange(str => {
     console.log(str)
     console.log(search(str))
+    
+    $search.replaceChildren(
+        ...search(str)
+            .map(x => x.join(" "))
+            .map(x => el("div", {
+                children: [
+                    x
+                ]
+            }))
+    )
 })
 
 document.addEventListener("selectionchange", () => {
