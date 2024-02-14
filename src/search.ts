60c254471a3742e780529a6fdc6ab53b341895c6
Gnlow <gnlow@protonmail.com>
2024-02-14 16:27:23 +0900

    asdf

--- src/search.ts
+++ src/search.ts
@@ -15,17 +15,30 @@ console.log(data)
 
 export const normalize = (str: string) => str.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase()
 
+const sort =
+<T>(...sorters: ((a: T, b: T) => -1 | 0 | 1)[]) =>
+(a: T, b: T) =>
+    sorters.find(sorter => sorter(a, b))?.(a, b) || 0
+
+const sortList = 
+(str: string) =>
+    sort<DictRow>(
+        (a, b) => 
+            normalize(a[1]).startsWith(normalize(str))
+                ? -1
+                : normalize(b[1]).startsWith(normalize(str))
+                    ? 1
+                    : 0,
+        (a, b) =>
+        a[0] < b[0]
+            ? -1
+            : 1
+    )
+
 export const search = (str: string) =>
     data.filter(([i, word]) =>
         normalize(word).includes(normalize(str))
     )
     .sort(
-        (a, b) => 
-            normalize(a[1]).startsWith(normalize(str))
-            ? -1
-            : normalize(b[1]).startsWith(normalize(str))
-                ? 1
-                : a[0] < b[0]
-                    ? -1
-                    : 1
+        sortList(str)
     )
