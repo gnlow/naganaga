a16842531a232cc16c595bef1f165215cd141536
Gnlow <gnlow@protonmail.com>
2024-02-24 17:52:58 +0900

    use hono

--- serve.ts
+++ serve.ts
@@ -1,1 +1,25 @@
-import "https://deno.land/x/tserve@0.1.3/mod.ts"
+import { transpile } from "https://deno.land/x/tserve@0.1.3/src/transpile.ts"
+
+import { Hono } from "https://deno.land/x/hono@v4.0.5/mod.ts"
+import { serveStatic } from "https://deno.land/x/hono@v4.0.5/middleware.ts"
+
+const app = new Hono()
+
+app.get("/src/:filename{.*\\.ts$}", async c => {
+    return await transpile("." + c.req.path)
+        .then(v => {
+            if (!v) throw v
+            return v
+        })
+        .then(v => c.body(
+            v,
+            200,
+            {
+                "content-type": "application/javascript",
+            }
+        ))
+})
+
+app.get("*", serveStatic())
+
+Deno.serve(app.fetch)
